c0dfca55bb4d92e06c9c57ac803d8095c2271022
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-07-15 22:35:29 +0900

    Revert "First-commit"
    This reverts commit f5fe163c1fa959d5d963452950fff9ba9fecd686.

--- spec/factories/comments.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :comment do
-    text {Faker::Lorem.sentence}
-    association :user
-    association :tweet 
-  end
-end
--- spec/factories/tweets.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :tweet do
-    text {Faker::Lorem.sentence}
-    image {Faker::Lorem.sentence}
-    association :user 
-  end
-end
